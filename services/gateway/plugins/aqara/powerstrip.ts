342436b6cd84dd08d526e595a527373b0db41695
Andrew Yang <ddvkid@gmail.com>
2018-08-05 01:10:35 +1000

    fix powerstrip status issue

--- services/gateway/plugins/aqara/powerstrip.ts
+++ services/gateway/plugins/aqara/powerstrip.ts
@@ -1,9 +1,9 @@
 import miio from "miio";
 import { mqttService } from "../../";
-import { Observable, Subject, fromEvent, from } from "rxjs";
+import { Observable, Subject, merge, fromEvent } from "rxjs";
 import { Plugin } from "../../services/plugin";
 import { IMqttMessage } from "../../types/mqtt.model";
-import { map, share } from "rxjs/operators";
+import { map, share, switchMap, delay } from "rxjs/operators";
 export default class implements Plugin {
   device: any;
   topic: string;
@@ -30,38 +30,28 @@ export default class implements Plugin {
     return this.$status.pipe(share());
   }
 
+  private getMqttMessage(packet: IMqttMessage) {
+    const { status } = JSON.parse(packet.payload.toString());
+    return status;
+  }
+
   private handleDevice = device => {
     console.log("Connected to", device);
     this.$health.next(true);
     if (device.matches("type:power-strip")) {
-      this.$("update")
-        .pipe(
-          map((packet: IMqttMessage) => {
-            const { status } = JSON.parse(packet.payload.toString());
-            return status;
-          })
+      merge(
+        fromEvent(device, "powerChanged").pipe(map(([status]) => ({ status }))),
+        this.$("update").pipe(
+          map(this.getMqttMessage),
+          switchMap(
+            status => device.setPower(status),
+            tartgetStatus => ({ status: tartgetStatus })
+          ),
+          delay(10)
         )
-        .subscribe(status => {
-          from(device.setPower(status)).subscribe();
-          // this.$status.next({ status });
-          // this.$status.next(state);
-          // from(device.power())
-          //   .pipe(map(status => ({ status })))
-          //   .subscribe(status => {
-          //     console.log(111, status);
-          //     this.$status.next(status);
-          //   });
-        });
-      // from(device.power())
-      //   .pipe(map(status => ({ status })))
-      //   .subscribe(status => this.$status.next(status));
-
-      fromEvent(device, "powerChanged")
-        .pipe(map(([status]) => ({ status })))
-        .subscribe(res => {
-          console.log("powerChanged", res);
-          this.$status.next(res);
-        });
+      ).subscribe(status => {
+        this.$status.next(status);
+      });
     }
   };
 }
